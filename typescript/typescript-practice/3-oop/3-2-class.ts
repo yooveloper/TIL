8d246b479ffd7c57908e039ef92f030ef53bf207
yooveloper <yooveloper@gmail.com>
2022-10-16 00:58:05 +0900

    ✅ add: class 를 활용해서 커피머신 만들기

---
+++ typescript/typescript-practice/3-oop/3-2-class.ts
@@ -0,0 +1,40 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAMM_PER_SHOT = 7; // class level
+    coffeeBeans: number = 0; // instance(object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(32);
+  console.log(maker);
+  const maker2 = new CoffeeMaker(14);
+  console.log(maker2);
+  const maker3 = CoffeeMaker.makeMachine(3);
+  console.log(maker3);
+
+  // 대표적으로 JS 에 Math 함수가 class 레벨이기 때문에 new 연산자로 인스턴스를 생성하지 않아도 사용 가능
+}
