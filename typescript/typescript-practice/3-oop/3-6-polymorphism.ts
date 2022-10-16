a4cab06dde8546a506e1ed15092c13d38af267bb
yooveloper <yooveloper@gmail.com>
2022-10-16 22:57:49 +0900

    ✅ add: 다형성 개념정리 필요

---
+++ typescript/typescript-practice/3-oop/3-6-polymorphism.ts
@@ -0,0 +1,109 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT = 7; // class level
+    private coffeeBeans: number = 0; // instance(object) level
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    // 외부에서 내부의 값을 변경하기 위한 메서드
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+
+    clean() {
+      console.log("cleaning the machine... 🧼");
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(12);
+  maker.makeCoffee(2);
+
+  class CaffeeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+
+    private steamMilk(): void {
+      console.log("Steaming some milk... 🥛");
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweeetCoffeeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const machines: CoffeeMaker[] = [
+    new CoffeeMachine(16),
+    new CaffeeLatteMachine(16, "1"),
+    new SweeetCoffeeMaker(16),
+    new CoffeeMachine(16),
+    new CaffeeLatteMachine(16, "1"),
+    new SweeetCoffeeMaker(16),
+  ];
+
+  machines.forEach((machine) => {
+    console.log("---------------------");
+    machine.makeCoffee(1);
+  });
+}
