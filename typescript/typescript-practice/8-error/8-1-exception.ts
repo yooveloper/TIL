c100d31811563d851ac12c77dffba980d3ca8ecf
yooveloper <yooveloper@gmail.com>
2022-10-17 16:19:59 +0900

    ✅ add: error handling 정리

---
+++ typescript/typescript-practice/8-error/8-1-exception.ts
@@ -0,0 +1,28 @@
+// Java : Exception
+// JavaScript: Error
+
+// Error(Exception) Handling: try -> catch > finally
+
+function readFile(fileName: string): string {
+  if (fileName === "not exist!💩") {
+    throw new Error(`file not exist! ${fileName}`);
+  }
+  return "file contents 📁";
+}
+
+function closeFile(fileName: string) {}
+
+function run() {
+  const fileName = "not exist!💩";
+  try {
+    console.log(readFile(fileName));
+  } catch (error) {
+    console.log(`catched!!`);
+    return;
+  } finally {
+    closeFile(fileName);
+    console.log("closed");
+  }
+}
+
+run();
