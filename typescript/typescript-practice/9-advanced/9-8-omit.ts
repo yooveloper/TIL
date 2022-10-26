7b70af407f6b8267fc7303049c4103fdfcffe87c
yooveloper <yooveloper@gmail.com>
2022-10-26 23:21:51 +0900

    ✅  Omit 정리 Pick과 반대

--- typescript/typescript-practice/9-advanced/9-8-omit.ts
+++ typescript/typescript-practice/9-advanced/9-8-omit.ts
@@ -1,4 +1,4 @@
-// 기존에 있는 type에서 내가 원한는것만 골라서 타입으로 만들고 싶을때 pick 사용
+// Pick 과 반대로 원하는것을 뺄 수 있다.
 type Video = {
   id: string;
   title: string;
@@ -6,7 +6,9 @@ type Video = {
   data: string;
 };
 
-type VideoMetadata = Pick<Video, "id" | "title">;
+// 기존에 Video 타입에서 url과 data를 빼고 사용
+// 기존 타입에 없는 key여도 전달 가능
+type VideoMetadata = Omit<Video, "url" | "data">;
 function getVideo(id: string): Video {
   return {
     id,
